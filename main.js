5663587d2b9b74dfdffada6f894a1becdcdcbff4
codeSTACKr <codestackr@gmail.com>
2020-11-19 09:09:33 -0600

    made responsive

---
+++ main.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
+const expandBtn = document.querySelectorAll(".expand-btn");
+
+// humburger toggle
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
+});
+
+// mobile menu expand
+expandBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("open");
+  });
+});
